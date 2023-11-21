911ab2859f9dabf76032a6a6e07970f3827a9f33
nng2 <nng2@ewu.edu>
2023-11-21 11:13:51 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     $url = 'https://example.com/api';
     $response = file_get_contents($url);
-    echo <h1>Messenger<h1>
-    echo <hr>
+    echo "<h1>Messenger<h1>"
+    echo "<hr>"
     echo $response;
+?>

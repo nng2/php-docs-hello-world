87bdc8d3aeaff2af0b81be39e0195ca0fcb375e4
nng2 <nng2@ewu.edu>
2023-11-21 11:10:35 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-
-echo "Update Testing... Again!";
+    $url = 'https://example.com/api';
+    $response = file_get_contents($url);
+    echo <h1>Messenger<h1>
+    echo <hr>
+    echo $response;
